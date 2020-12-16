b380c9857cc900d7e3611e519ae247f69a658132
FRZY03 <75489871+FRZY03@users.noreply.github.com>
2020-12-16 11:39:13 +0800

    Add files via upload

---
+++ shrp_env.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+export SHRP_MAINTAINER="epicX"
+export SHRP_DEVICE="c103"
+export SHRP_EDL_MODE="1"
+export SHRP_EXTERNAL="/external_sd"
+export SHRP_INTERNAL="/sdcard"
+export SHRP_OTG="/usb-otg"
+export SHRP_FLASH="1"
+export SHRP_FLASH_MAX_BRIGHTNESS="200"
+export SHRP_FONP_1="/sys/class/leds/led:torch_0/brightness"
+export SHRP_FONP_2="/sys/class/leds/led:torch_1/brightness"
+export SHRP_FONP_3="/sys/class/leds/led:switch/brightness"
+export SHRP_REC="/dev/block/bootdevice/by-name/recovery"
